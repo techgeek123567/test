57e672ef786cdd9307363e19a631285f22d245c4
techgeek123567 <127588567+techgeek123567@users.noreply.github.com>
2023-03-11 14:22:01 +0400

    Create main.tf

---
+++ assessment-1/gke/main.tf
@@ -0,0 +1,23 @@
+module "dev_cluster" {
+    source     = "./module"
+    env_name   = "dev"
+    project_id = "${var.project_id}"
+    instance_type = "e2-medium"
+}
+ 
+module "staging_cluster" {
+    source     = "./module"
+    env_name   = "staging"
+    project_id = "${var.project_id}"
+    instance_type = "e2-medium"
+}
+ 
+module "prod_cluster" {
+    source     = "./module"
+    env_name   = "prod"
+    project_id = "${var.project_id}"
+    instance_type = "n2-highmem-2"
+}
+ 
+variable "project_id" {
+  description = "The project ID to host the cluster in"
