62ec8a15036730998e8ea207defae0d3cfd412cc
techgeek123567 <127588567+techgeek123567@users.noreply.github.com>
2023-03-11 14:26:48 +0400

    Create variables.tf

---
+++ assessment-1/mysql/modules/variables.tf
@@ -0,0 +1,47 @@
+variable "project-id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "region-zone" {
+  type = string
+}
+
+variable "database-version" {
+  type = string
+}
+
+variable "tier" {
+  type = string
+}
+
+variable "authorized-networks" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "database-name" {
+  type = string
+}
+
+variable "db-username" {
+  type = string
+}
+
+variable "charset" {
+  type = string
+}
+
+variable "collation" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
