95854b6508d7eb492c5b53df70dcaea758226d8b
techgeek123567 <127588567+techgeek123567@users.noreply.github.com>
2023-03-12 11:32:44 +0400

    Create main.tf

---
+++ assesment1-b/mysql/main.tf
@@ -0,0 +1,14 @@
+module "mysql" {
+  source              = "./custom-module"
+  project-id          = var.project-id
+  region              = var.region
+  region-zone         = var.region-zone
+  database-version    = var.database-version
+  tier                = var.tier
+  authorized-networks = var.authorized-networks
+  zone                = var.zone
+  database-name       = var.database-name
+  db-username         = var.db-username
+  charset             = var.charset
+  password            = var.password
+}
