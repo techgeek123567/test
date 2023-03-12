9e1d26ab9697cda2f00edd3224efdd3a9e04eb6a
techgeek123567 <127588567+techgeek123567@users.noreply.github.com>
2023-03-12 11:28:53 +0400

    Create variables.tf

---
+++ assesment1-b/application-server/modules/variables.tf
@@ -0,0 +1,47 @@
+variable "project-id" {
+  type = string
+}
+
+variable "service_account" {
+  type = string
+}
+
+variable "service_account_id" {
+  type = string
+}
+
+variable "instance_name" {
+  type = string
+}
+
+variable "machine-type" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "region-zone" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
+
+variable "metadata-script" {
+  type = string
+}
+
+variable "google_service_account-email" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
+variable "subnetwork" {
+  type = string
+}
